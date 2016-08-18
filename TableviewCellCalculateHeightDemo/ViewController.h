c9258f997dd83e1fb95af61caeaefd2cc43bf649
MHZ <wlsxmahaozhe@163.com>
2016-08-18 10:10:41 +0800

    Initial Commit

---
+++ TableviewCellCalculateHeightDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableviewCellCalculateHeightDemo
+//
+//  Created by 马浩哲 on 16/8/18.
+//  Copyright © 2016年 junanxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TableviewCellCalculateHeightDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TableviewCellCalculateHeightDemo
+//
+//  Created by 马浩哲 on 16/8/18.
+//  Copyright © 2016年 junanxin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
